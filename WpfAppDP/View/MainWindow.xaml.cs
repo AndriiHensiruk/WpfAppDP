66ab665cb70020fd21717bc22bfb945b52cbc934
AndriiHensiruk <Hensiruk@gmail.com>
2023-03-25 14:48:20 +0200

    Add relay Comand for open windows

--- WpfAppDP/View/MainWindow.xaml.cs
+++ WpfAppDP/View/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfAppDP.ViewModel;
 
 namespace WpfAppDP.View
 {
@@ -20,9 +21,12 @@ namespace WpfAppDP.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static ListView AllUsersView;
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new DataManageVM();
+            AllUsersView = ViewAllUsers;
         }
     }
 }
--- WpfAppDP/ViewModel/DataMangerVW.cs
+++ WpfAppDP/ViewModel/DataMangerVW.cs
@@ -1,12 +1,245 @@
-﻿using System;
+﻿using WpfAppDP.Model;
+using WpfAppDP.View;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace WpfAppDP.ViewModel
 {
-    class DataMangerVW
+    public class DataManageVM : INotifyPropertyChanged
     {
+        
+        //все сотрудники
+        private List<User> allUsers = DataWorker.GetAllUsers();
+        public List<User> AllUsers
+        {
+            get
+            {
+                return allUsers;
+            }
+            private set
+            {
+                allUsers = value;
+                NotifyPropertyChanged("AllUsers");
+            }
+        }
+
+       
+        //свойства для сотрудника
+        public static string UserName { get; set; }
+        public static string UserSurName { get; set; }
+        public static string UserPhone { get; set; }
+        public static string UserComment { get; set; }
+
+        //свойства для выделенных элементов
+        public TabItem SelectedTabItem { get; set; }
+        public static User SelectedUser { get; set; }
+
+        #region COMMANDS TO ADD
+        private RelayCommand addNewUser;
+        public RelayCommand AddNewUser
+        {
+            get
+            {
+                return addNewUser ?? new RelayCommand(obj =>
+                {
+                    Window wnd = obj as Window;
+                    string resultStr = "";
+                    if (UserName == null || UserName.Replace(" ", "").Length == 0)
+                    {
+                        SetRedBlockControll(wnd, "NameBlock");
+                    }
+                    if (UserSurName == null || UserSurName.Replace(" ", "").Length == 0)
+                    {
+                        SetRedBlockControll(wnd, "SurNameBlock");
+                    }
+                    //if (UserPhone == null || UserPhone.Replace(" ", "").Length == 0)
+                    //{
+                    //    SetRedBlockControll(wnd, "SurNameBlock");
+                    //}
+                    if (UserComment == null)
+                    {
+                        MessageBox.Show("Укажите позицию");
+                    }
+                    else
+                    {
+                        resultStr = DataWorker.CreateUser(UserName, UserSurName, UserPhone, UserComment);
+                        UpdateAllDataView();
+
+                        ShowMessageToUser(resultStr);
+                        SetNullValuesToProperties();
+                        wnd.Close();
+                    }
+                }
+                );
+            }
+        }
+
+        #endregion
+
+        private RelayCommand deleteItem;
+        public RelayCommand DeleteItem
+        {
+            get
+            {
+                return deleteItem ?? new RelayCommand(obj =>
+                {
+                    string resultStr = "Ничего не выбрано";
+                    //если сотрудник
+                    if (SelectedTabItem.Name == "UsersTab" && SelectedUser != null)
+                    {
+                        resultStr = DataWorker.DeleteUser(SelectedUser);
+                        UpdateAllDataView();
+                    }
+                   
+                    //обновление
+                    SetNullValuesToProperties();
+                    ShowMessageToUser(resultStr);
+                }
+                    );
+            }
+        }
+
+        #region EDIT COMMANDS
+        private RelayCommand editUser;
+        public RelayCommand EditUser
+        {
+            get
+            {
+                return editUser ?? new RelayCommand(obj =>
+                {
+                    Window window = obj as Window;
+                    string resultStr = "Не выбран сотрудник";
+                    string noPositionStr = "Не выбрана новая должность";
+                    if (SelectedUser != null)
+                    {
+                        if (UserComment != null)
+                        {
+                            resultStr = DataWorker.EditUser(SelectedUser, UserName, UserSurName, UserPhone, UserComment);
+
+                            UpdateAllDataView();
+                            SetNullValuesToProperties();
+                            ShowMessageToUser(resultStr);
+                            window.Close();
+                        }
+                        else ShowMessageToUser(noPositionStr);
+                    }
+                    else ShowMessageToUser(resultStr);
+
+                }
+                );
+            }
+        }
+        
+        #endregion
+
+        #region COMMANDS TO OPEN WINDOWS
+
+        private RelayCommand openAddNewUserWnd;
+        public RelayCommand OpenAddNewUserWnd
+        {
+            get
+            {
+                return openAddNewUserWnd ?? new RelayCommand(obj =>
+                {
+                    OpenAddUserWindowMethod();
+                }
+                );
+            }
+        }
+        private RelayCommand openEditItemWnd;
+        public RelayCommand OpenEditItemWnd
+        {
+            get
+            {
+                return openEditItemWnd ?? new RelayCommand(obj =>
+                {
+                    string resultStr = "Ничего не выбрано";
+                    //если сотрудник
+                    if (SelectedTabItem.Name == "UsersTab" && SelectedUser != null)
+                    {
+                        OpenEditUserWindowMethod(SelectedUser);
+                    }
+                }
+                    );
+            }
+        }
+        #endregion
+
+        #region METHODS TO OPEN WINDOW
+       
+        private void OpenAddUserWindowMethod()
+        {
+            AddUser newUserWindow = new AddUser();
+            SetCenterPositionAndOpen(newUserWindow);
+        }
+
+        //окна редактирования
+        
+        private void OpenEditUserWindowMethod(User user)
+        {
+            EditUser editUserWindow = new EditUser(user);
+            SetCenterPositionAndOpen(editUserWindow);
+        }
+        private void SetCenterPositionAndOpen(Window window)
+        {
+            window.Owner = Application.Current.MainWindow;
+            window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            window.ShowDialog();
+        }
+        #endregion
+
+        private void SetRedBlockControll(Window wnd, string blockName)
+        {
+            Control block = wnd.FindName(blockName) as Control;
+            block.BorderBrush = Brushes.Red;
+        }
+
+        #region UPDATE VIEWS
+        private void SetNullValuesToProperties()
+        {
+            //для пользователя
+            UserName = null;
+            UserSurName = null;
+            UserPhone = null;
+            UserComment = null;
+            
+        }
+        private void UpdateAllDataView()
+        {
+            UpdateAllUsersView();
+        }
+
+       
+        private void UpdateAllUsersView()
+        {
+            AllUsers = DataWorker.GetAllUsers();
+            MainWindow.AllUsersView.ItemsSource = null;
+            MainWindow.AllUsersView.Items.Clear();
+            MainWindow.AllUsersView.ItemsSource = AllUsers;
+            MainWindow.AllUsersView.Items.Refresh();
+        }
+        #endregion
+
+        private void ShowMessageToUser(string message)
+        {
+            MessageView messageView = new MessageView(message);
+            SetCenterPositionAndOpen(messageView);
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
